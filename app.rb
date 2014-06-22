ccff680952b12253d0b0fd546356e43b9fe4491a
alitaso345 <alice.maru345@gmail.com>
2014-06-22 11:49:56 +0900

    spec6:第一引数はidとすることにした

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'yaml'
 require_relative 'lib/Recipe'
 
 path = File.join(__dir__, 'recipe.yml')
+
 recipes = YAML.load(open(path))["recipes"]
 
-Recipe.new.put_recipe_name(recipes)
+#第一引数にはidの意味を持たせることとする
+id = ARGV[0].to_i
+
+Recipe.new.put_recipe_name(recipes, id)
