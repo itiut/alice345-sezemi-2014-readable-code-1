c6afa0194b40aa5a90dfcf91043b8fb88278551c
alitaso345 <alice.maru345@gmail.com>
2014-06-22 12:02:38 +0900

    spec7:レシピ情報でURLも出力できるように変更
    仕様7まで実装完了

--- app.rb
+++ app.rb
@@ -6,6 +6,6 @@ path = File.join(__dir__, 'recipe.yml')
 recipes = YAML.load(open(path))["recipes"]
 
 #第一引数にはidの意味を持たせることとする
-id = ARGV[0].to_i
+id = ARGV[0]
 
 Recipe.new.put_recipe_name(recipes, id)
--- lib/Recipe.rb
+++ lib/Recipe.rb
@@ -7,11 +7,11 @@ class Recipe
     #idが入力にあるか否かで表示を変える
     if(id)
       recipes.each do |recipe|
-        puts "#{recipe["id"]}:#{recipe["name"]}" if id == recipe["id"]
+        puts "#{recipe["id"]}:#{recipe["name"]} #{recipe["url"]}" if id == recipe["id"].to_s
       end
     else
       recipes.each do |recipe|
-        puts "#{recipe["id"]}:#{recipe["name"]}"
+        puts "#{recipe["id"]}:#{recipe["name"]} #{recipe["url"]}"
       end
     end
   end
