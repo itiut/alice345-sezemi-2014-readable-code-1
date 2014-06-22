1681164868d9ac9bb86c0d549fbd42498f267404
alitaso345 <alice.maru345@gmail.com>
2014-06-22 11:25:49 +0900

    spec4:複数のレシピがあるので，複数形の名前に変更

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'yaml'
 require_relative 'lib/Recipe'
 
 path = File.join(__dir__, 'recipe.yml')
-recipes = YAML.load(open(path))["recipe"]
+recipes = YAML.load(open(path))["recipes"]
 
 Recipe.new.put_recipe_name(recipes)
