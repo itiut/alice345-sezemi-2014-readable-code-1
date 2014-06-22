4f6c547176e1b49ae6af617c33346bd2a56a920d
alitaso345 <alice.maru345@gmail.com>
2014-06-22 11:50:27 +0900

    spec6:IDを指定したレシピだけ出力するような条件分岐を追加

--- lib/Recipe.rb
+++ lib/Recipe.rb
@@ -3,9 +3,16 @@ class Recipe
 
   end
 
-  def put_recipe_name(recipes)
-    recipes.each do |recipe|
-      puts "#{recipe["id"]}:#{recipe["name"]}"
+  def put_recipe_name(recipes, id)
+    #idが入力にあるか否かで表示を変える
+    if(id)
+      recipes.each do |recipe|
+        puts "#{recipe["id"]}:#{recipe["name"]}" if id == recipe["id"]
+      end
+    else
+      recipes.each do |recipe|
+        puts "#{recipe["id"]}:#{recipe["name"]}"
+      end
     end
   end
 end
